ea7f8a31c89f0be9c546cd5eee2f2665e964e939
“yui-mu”　 <yuimuto.0528@gmail.com>
2025-04-10 21:06:55 +0900

    register.html、フォーム、ビューの作成完了

--- epilog/Epilogapp/views.py
+++ epilog/Epilogapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # 登録後にログインページへ
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
