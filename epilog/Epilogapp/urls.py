3aa5297b6defa441ac23bb3f5c6574d4fc4d1862
“yui-mu”　 <yuimuto.0528@gmail.com>
2025-04-12 10:23:21 +0900

    ログイン画面の追加

--- epilog/Epilogapp/urls.py
+++ epilog/Epilogapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import register_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
+     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
