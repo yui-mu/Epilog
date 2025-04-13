6d149194ea80032a0d37e1e898f33d8460786251
“yui-mu”　 <yuimuto.0528@gmail.com>
2025-04-13 09:29:07 +0900

    トップページを追加し、ログイン・登録への導線を設置

--- epilog/Epilogapp/urls.py
+++ epilog/Epilogapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register_view, home_view
+from .views import register_view, home_view, top_view
 
 urlpatterns = [
+    path('', top_view, name='top'),
     path('register/', register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- epilog/Epilogapp/views.py
+++ epilog/Epilogapp/views.py
@@ -16,3 +16,6 @@ def register_view(request):
 @login_required
 def home_view(request):
     return render(request, 'home.html')
+
+def top_view(request):
+    return render(request, 'top.html')
