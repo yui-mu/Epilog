bea1f0f5288b76907f6f87baf3b2f8048afe8981
“yui-mu”　 <yuimuto.0528@gmail.com>
2025-05-13 17:57:59 +0900

    Add STATICFILES_DIRS and STATIC_ROOT for deployment

--- epilog/epilog/settings.py
+++ epilog/epilog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,3 +133,7 @@ DEFAULT_FROM_EMAIL = 'noreply@epilogapp.com'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGOUT_REDIRECT_URL = 'logged_out'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Epilogapp', 'static'),
+]
